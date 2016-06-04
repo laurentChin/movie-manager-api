753229560531b09370df86d5301d0eb6f102dde4
Laurent Janet <laurent.janet@gmail.com>
2016-06-04 16:05:32 +0200

    initialise testing structure

---
+++ test/bootstrap.test.js
@@ -0,0 +1,24 @@
+var sails = require('sails');
+
+before(function(done) {
+
+  // Increase the Mocha timeout so that Sails has enough time to lift.
+  this.timeout(5000);
+
+  sails.lift({
+    // configuration for testing purposes
+    models : {
+      migrate : 'drop'
+    },
+    port: 1789
+  }, function(err, server) {
+    if (err) return done(err);
+    // here you can load fixtures, etc.
+    done(err, sails);
+  });
+});
+
+after(function(done) {
+  // here you can clear fixtures, etc.
+  sails.lower(done);
+});
---
+++ test/integration/models/Movie.test.js
@@ -0,0 +1,9 @@
+describe('MovieModel', () => {
+  describe('#find()', () => {
+    it('should check find function', (done) => {
+      Movie.find()
+        .then(results => done())
+        .catch(done);
+    });
+  });
+});
