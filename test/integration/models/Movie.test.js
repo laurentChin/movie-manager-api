ae2af7899017856b995a1652dac21f4289312769
Laurent Janet <laurent.janet@gmail.com>
2016-06-05 20:50:28 +0200

    add test for Movie update()

--- test/integration/models/Movie.test.js
+++ test/integration/models/Movie.test.js
@@ -1,18 +1,19 @@
 'use strict';
 
 var should = require('chai').should();
+let movie = {
+  title: 'Will Hunting',
+  originalTitle: 'Good Will Hunting',
+  theaterReleaseDate: new Date('01 January 1997 00:00 UTC'),
+  duration: 126
+};
 
 describe('MovieModel', () => {
   describe('#create()', () => {
     it('should check create function', done => {
       Movie.should.have.property('create');
       Movie
-        .create({
-          title: 'Will Hunting',
-          originalTitle: 'Good Will Hunting',
-          theaterReleaseDate: new Date('01 January 1997 00:00 UTC'),
-          duration: 126
-        })
+        .create(movie)
         .then(result => {
           result.should.have.property('id');
           result.should.have.property('createdAt');
@@ -31,6 +32,24 @@ describe('MovieModel', () => {
     it('should check find function', done => {
       Movie.find()
         .then(results => done())
+        .catch(done);
+    });
+  });
+
+  describe('#update()', () => {
+    it('should check update function', done => {
+      Movie.create(movie)
+        .then(result => {
+          Movie.update(
+            result.id,
+            {
+              originalTitle: 'Not so good Will Hunting'
+            }
+          ).then(updateResult => {
+            updateResult[0].originalTitle.should.equal('Not so good Will Hunting');
+            done();
+          });
+        })
         .catch(done);
     });
   });
