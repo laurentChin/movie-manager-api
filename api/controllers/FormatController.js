0e53fecf7bcfc030a080a6f2e972495c1d16caa3
Laurent Janet <laurent.janet@gmail.com>
2016-06-05 22:03:51 +0200

    initialise Format api

---
+++ api/controllers/FormatController.js
@@ -0,0 +1,9 @@
+/**
+ * FormatController
+ *
+ * @description :: Server-side logic for managing formats
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
+
---
+++ test/integration/models/Format.test.js
@@ -0,0 +1,3 @@
+'use strict';
+
+let should = require('chai').should();
