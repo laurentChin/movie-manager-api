ba7c13a6b1fd43851a50c73e6ffc401a2c47e5ac
Laurent Janet <laurent.janet@gmail.com>
2017-11-29 22:00:38 +0100

    cs

--- tests/security/facebookProvider.js
+++ tests/security/facebookProvider.js
@@ -1,6 +1,14 @@
 import test from 'ava';
 import facebookProvider, {buildAccessTokenRequest, buildAccessTokenRequestUrl } from '../../src/security/facebookProvider';
 
+test('facebookProvider default export must an object', t => {
+  t.is('object', typeof facebookProvider.default);
+});
+
+test('facebookProvider default export must own a buildAccessTokenRequest property', t => {
+  t.truthy(facebookProvider.default.hasOwnProperty('buildAccessTokenRequest'));
+});
+
 test('buildAccessTokenRequest must build a valid facebook access token request', t => {
   const expected = 'https://graph.facebook.com/v2.11/oauth/access_token?client_id=app-id&redirect_uri=redirect-uri&client_secret=app-secret&code=code-parameter';
   t.is(
@@ -23,14 +31,6 @@ test('buildAccessTokenRequestUrl url must build a valid facebook token url', t =
     expected,
     buildAccessTokenRequestUrl('https://graph.facebook.com', 'v2.11/oauth/access_token')
   );
-});
-
-test('facebookProvider default export must an object', t => {
-  t.is('object', typeof facebookProvider.default);
-});
-
-test('facebookProvider default export must own a buildAccessTokenRequest property', t => {
-  t.truthy(facebookProvider.default.hasOwnProperty('buildAccessTokenRequest'));
 });
 
 test('facebookProvider default export must own an authenticate property', t => {
