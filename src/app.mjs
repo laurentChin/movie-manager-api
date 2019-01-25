4a6a452b62b9d8b7ba841fc7143be1a13c2a49b2
Laurent Janet <laurent.janet@gmail.com>
2019-01-25 14:33:50 +0100

    cs

--- src/app.mjs
+++ src/app.mjs
@@ -1,32 +1,40 @@
-import express from 'express';
-import jwt from 'express-jwt';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import path from 'path';
-import fs from 'fs';
+import express from "express";
+import jwt from "express-jwt";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import fs from "fs";
 
-import environment from '../environment';
+import environment from "../environment";
 
-import { User, Movie, Format } from './models/index';
-import { securityRouterFactory } from './security/index';
-import { movieRouterFactory } from './movie/index';
-import { formatRouterFactory } from './format/index';
+import { User, Movie, Format } from "./models/index";
+import { securityRouterFactory } from "./security/index";
+import { movieRouterFactory } from "./movie/index";
+import { formatRouterFactory } from "./format/index";
 
 const app = express();
 
 app
-  .use(bodyParser.json({limit: '8mb'}))
+  .use(bodyParser.json({ limit: "8mb" }))
   .use(cors())
-  .use(jwt({secret: environment.jwtSecretKey}).unless({path: [/^\/security/, /^\/uploads/]}))
-  .use('/security', securityRouterFactory(express.Router(), User, environment.jwtSecretKey))
-  .use('/movies', movieRouterFactory(express.Router(), Movie, User, Format))
-  .use('/formats', formatRouterFactory(express.Router(), Format))
-  .use(express.static('public', {fallthrough: false}))
+  .use(
+    jwt({ secret: environment.jwtSecretKey }).unless({
+      path: [/^\/security/, /^\/uploads/]
+    })
+  )
+  .use(
+    "/security",
+    securityRouterFactory(express.Router(), User, environment.jwtSecretKey)
+  )
+  .use("/movies", movieRouterFactory(express.Router(), Movie, User, Format))
+  .use("/formats", formatRouterFactory(express.Router(), Format))
+  .use(express.static("public", { fallthrough: false }))
   .use((err, request, response, next) => {
     if (/^\/uploads/.test(request.url)) {
-      const posterFallback = fs.createReadStream(path.join(process.env.PWD, 'public', 'assets', 'poster-placeholder.png'));
-      posterFallback
-        .pipe(response);
+      const posterFallback = fs.createReadStream(
+        path.join(process.env.PWD, "public", "assets", "poster-placeholder.png")
+      );
+      posterFallback.pipe(response);
     } else {
       next(err);
     }
